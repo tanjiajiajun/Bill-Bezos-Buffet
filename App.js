55961a0db1d105da10f8526fad8cae01ae49d96b
tanjiajiajun <e0725056@u.nus.edu>
2022-05-24 02:09:39 +0800

    working alphawantage API and static graph produced

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import HomeScreen from './src/screens/HomeScreen';
+import Stock from './src/components/Stock';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Stock/>
     </View>
   );
 }
---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text, Button } from 'react-native';
+
+function HomeScreen(props) {
+    return (
+        <View>
+            <Text> Hello World</Text>
+            <Button title='Start!'></Button>
+        </View>
+    );
+}
+
+export default HomeScreen;

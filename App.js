23ecc5b2b8a03d0cc900f282bd495e068413e9a1
tanjiajiajun <e0725056@u.nus.edu>
2022-06-06 19:00:39 +0800

    start/stop

--- App.js
+++ App.js
@@ -9,18 +9,19 @@ import { createStackNavigator } from '@react-navigation/stack';
 import RegisterScreen from './src/screens/RegisterScreen'
 import LoginScreen from './src/screens/LoginScreen';
 import HomeScreen from './src/screens/HomeScreen'
+import AnimatedStock from './src/components/AnimatedStock';
 
 
 const Stack = createStackNavigator();
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false}}/>
-        <Stack.Screen name="Register" component={RegisterScreen}/>
-        <Stack.Screen name="Home" component={HomeScreen}/>
-      </Stack.Navigator>
-    </NavigationContainer> 
-
+    // <NavigationContainer>
+    //   <Stack.Navigator initialRouteName="Login">
+    //     <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false}}/>
+    //     <Stack.Screen name="Register" component={RegisterScreen}/>
+    //     <Stack.Screen name="Home" component={HomeScreen}/>
+    //   </Stack.Navigator>
+    // </NavigationContainer> 
+    <AnimatedStock/>
   );
 }
--- src/components/AnimatedStock.js
+++ src/components/AnimatedStock.js
@@ -1,69 +1,126 @@
 import React, { useState, useEffect } from 'react';
-import {View, Text, TouchableHighlight} from 'react-native'
+import {View, Text, TouchableOpacity, SafeAreaView, StyleSheet} from 'react-native'
 
 import { Path } from 'react-native-svg'
-import { AreaChart, Grid } from 'react-native-svg-charts'
+import { AreaChart, Grid, YAxis } from 'react-native-svg-charts'
 import * as shape from 'd3-shape'
+
+
 
 function AnimatedStock(props) {
 
     const data = require('../data/AAPL.json')
+    const [randomint, setRandomint] = useState(0)
+
+    useEffect( () => {
+        setRandomint(Math.floor(Math.random() * (data.length - 300)))
+    }, [])
+
+    const gameArray = data.slice(randomint, randomint + 301)
+
+    const n = gameArray[0]['Adj Close']
+    
     let xData = []
-    let yData = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    let yData = [
+        n, n, n, n, n, n, n, n, n, n,
+        n, n, n, n, n, n, n, n, n, n,
+        n, n, n, n, n, n, n, n, n, n,
+    ]
 
     const [count, setCount] = useState(0)
     const [yList, setyList] = useState(yData)
-    const [toggle, setToggle] = useState(false)
+    const [start, setStart] = useState(false)
 
 
-    
+
     const onpress = () => {
-        setToggle(prevToggle => !prevToggle)
-        console.log(toggle)
+        setStart(prevStart => !prevStart)
+        console.log(start)
+
     }
 
-    const textValue = toggle ? 'ON' : 'OFF'
+    // const textValue = toggle ? 'ON' : 'OFF'
 
-    //if (toggle === true) {
     useEffect( ()=> {
-        const id = setInterval(() => setCount(prevCount => prevCount + 1),100);
-        const iv = setInterval(() => setyList(prevyList => [...prevyList, data[count]["Adj Close"]].slice(1)),100)
-        console.log(yList)
+        var id;
+        var iv;
+        if (count == 300) {
+            alert('game ended')
+        }
+        else if (start == false) {
+            console.log('havent start')
+        }
+        else if (start == true){
+            var id = setInterval(() => setCount(prevCount => prevCount + 1),100);
+            var iv = setInterval(() => setyList(prevyList => [...prevyList, gameArray[count]["Adj Close"]].slice(1)),100)
+        }
+        //console.log(yList) //debugging purpose
         return () => {
             clearInterval(id)
             clearInterval(iv)
             }
 
-        }, [yList])
-    //}
+        }, [yList, start])
+    
     const Line = ({ line }) => (
         <Path
             key={'line'}
             d={line}
             stroke={'rgb(134, 65, 244)'}
             fill={'none'}
-        />)
+        >
+        </Path>
+        )
 
 
     return (
-    <View style={{width:'100%'}}>
-        <Text>Hello!</Text>
-        <AreaChart
-                style={{ height: 200 }}
+    <SafeAreaView style={styles.container}>
+        <Text>This is the graph below!</Text>
+        <View>
+            <AreaChart
+                style={{
+                    height: 500 ,
+                    width: '95%',
+
+                }}
                 data={yList}
                 contentInset={{ top: 30, bottom: 30 }}
                 curve={shape.curveNatural}
                 svg={{ fill: 'rgba(134, 65, 244, 0.2)' }}
-                animate={true}
-                animationDuration={10}
             >
-    
-                <Line/>
+            <Line/>
             </AreaChart>
-                <Grid/>
 
-    </View>
+        </View>
+        <TouchableOpacity style={styles.button} onPress={onpress}>
+            <Text>Start</Text>
+        </TouchableOpacity>
+        <View>
+        <TouchableOpacity>
+            <Text>Long</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity>
+            <Text>Short</Text>
+        </TouchableOpacity>
+        </View>
+    </SafeAreaView>
     );
 }
 
+const styles = StyleSheet.create({
+    container: {
+        width: '100%',
+    },
+    button: {
+        backgroundColor: '#0782F9',
+        width: '60%',
+        padding: 15,
+        margin: 40,
+        borderRadius: 10,
+        alignItems: 'center',
+        
+      }
+    }   
+)
 export default AnimatedStock;

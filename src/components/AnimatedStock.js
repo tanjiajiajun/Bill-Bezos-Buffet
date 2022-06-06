b447502146a087e7055f8f29d81e55d024737726
tanjiajiajun <e0725056@u.nus.edu>
2022-06-07 01:18:11 +0800

    minimum viable product

--- src/components/AnimatedStock.js
+++ src/components/AnimatedStock.js
@@ -2,8 +2,8 @@ import React, { useState, useEffect } from 'react';
 import {View, Text, TouchableOpacity, SafeAreaView, StyleSheet} from 'react-native'
 
 import { Path } from 'react-native-svg'
-import { AreaChart, Grid, YAxis } from 'react-native-svg-charts'
-import * as shape from 'd3-shape'
+import { AreaChart, YAxis } from 'react-native-svg-charts'
+
 
 
 
@@ -16,27 +16,25 @@ function AnimatedStock(props) {
         setRandomint(Math.floor(Math.random() * (data.length - 300)))
     }, [])
 
-    const gameArray = data.slice(randomint, randomint + 301)
+    const gameArray = data.slice(randomint, randomint + 300)
 
     const n = gameArray[0]['Adj Close']
     
     let xData = []
     let yData = [
-        n, n, n, n, n, n, n, n, n, n,
-        n, n, n, n, n, n, n, n, n, n,
-        n, n, n, n, n, n, n, n, n, n,
+        n,n,n,n,n,n,n,n,n,n,
+        n,n,n,n,n,n,n,n,n,n,
+        n,n,n,n,n,n,n,n,n,n,
     ]
 
     const [count, setCount] = useState(0)
     const [yList, setyList] = useState(yData)
     const [start, setStart] = useState(false)
-
+    
 
 
     const onpress = () => {
         setStart(prevStart => !prevStart)
-        console.log(start)
-
     }
 
     // const textValue = toggle ? 'ON' : 'OFF'
@@ -62,65 +60,132 @@ function AnimatedStock(props) {
 
         }, [yList, start])
     
+
+    const [amount, setAmount] = useState(10000)
+    const [buyingPrice, setBuyingPrice] = useState(0)
+    const [sellingPrice, setSellingPrice] = useState(0)
+    const [noShares, setNoShares] = useState(0)
+
+    useEffect( () => {
+        console.log(`u bought at ${buyingPrice}`)
+        setNoShares(Math.floor(amount/buyingPrice))
+        console.log(`u bought ${Math.floor(amount/buyingPrice)} shares`)
+        }, [buyingPrice]
+    )
+
+    useEffect( () => {
+        console.log(`u sold at ${sellingPrice}`)
+        console.log(`profit per share: ${sellingPrice-buyingPrice}`)
+        console.log(`total earnings for the trade: ${(sellingPrice-buyingPrice)*noShares}`)
+        setAmount(prev => prev + (sellingPrice-buyingPrice)*noShares )
+        }, [sellingPrice]
+    )
+
+    const handleLongOnPressIn = () => {
+        setBuyingPrice(yList[yList.length-1])
+    }
+    const handleLongOnPressOut = () => {
+        setSellingPrice(yList[yList.length-1])
+    }
+
+
+
+    const handleShortOnPressIn = () => {
+        console.log(yList[yList.length-1])
+    }
+    const handleShortOnPressOut = () => {
+        console.log(yList[yList.length-1])
+    }
+
+    // For graphing of line
     const Line = ({ line }) => (
         <Path
-            key={'line'}
-            d={line}
-            stroke={'rgb(134, 65, 244)'}
-            fill={'none'}
-        >
-        </Path>
-        )
-
-
+            d={ line }
+            stroke={ 'rgba(134, 65, 244)' }
+            fill={ 'none' }
+        />
+    )
     return (
     <SafeAreaView style={styles.container}>
-        <Text>This is the graph below!</Text>
-        <View>
-            <AreaChart
-                style={{
-                    height: 500 ,
-                    width: '95%',
-
-                }}
+        <Text style={styles.text}>Account balance: ${amount}</Text>
+        <View style={{ height: 500, flexDirection: 'row-reverse' }}>
+            <YAxis
                 data={yList}
-                contentInset={{ top: 30, bottom: 30 }}
-                curve={shape.curveNatural}
-                svg={{ fill: 'rgba(134, 65, 244, 0.2)' }}
-            >
-            <Line/>
+                contentInset={{ top: 20, bottom: 20 }}
+                svg={{ fill: 'grey', fontSize: 10,}}
+                numberOfTicks={10}
+                formatLabel={(value) => `$${value}`}
+            />
+            <AreaChart
+                style={{flex: 1}}
+                data={yList}
+                contentInset={{ top: 20, bottom: 20 }}
+                svg={{ fill: 'rgba(134, 65, 244, 0.2)' }}>
+                <Line/>
             </AreaChart>
-
         </View>
+
         <TouchableOpacity style={styles.button} onPress={onpress}>
             <Text>Start</Text>
         </TouchableOpacity>
-        <View>
-        <TouchableOpacity>
-            <Text>Long</Text>
-        </TouchableOpacity>
 
-        <TouchableOpacity>
-            <Text>Short</Text>
-        </TouchableOpacity>
+        <View style = {styles.longshotview}>
+            <TouchableOpacity style={styles.long}
+            onPressIn={handleLongOnPressIn} 
+            onPressOut={handleLongOnPressOut}>
+                <Text>Long</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style= {styles.short}>
+                <Text>Short</Text>
+            </TouchableOpacity>
         </View>
+
     </SafeAreaView>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
+        height: '100%',
         width: '100%',
+        backgroundColor:'black'
+    },
+    text: {
+        fontSize:30,
+        color:'white'
+
     },
     button: {
-        backgroundColor: '#0782F9',
+        backgroundColor: 'white',
         width: '60%',
         padding: 15,
         margin: 40,
+        marginBottom:10,
         borderRadius: 10,
         alignItems: 'center',
+        alignSelf: 'center',
         
-      }
+      },
+    longshotview: {
+        flexDirection:'row',
+        justifyContent:'center'
+    },
+    long: {
+        backgroundColor: '#32CD32',
+        width: '40%',
+        padding: 15,
+        borderRadius: 10,
+        alignItems: 'center',
+        margin: 10
+    },
+    short: {
+        backgroundColor: '#DC143C',
+        width: '40%',
+        padding: 15,
+        borderRadius: 10,
+        alignItems: 'center',
+        margin: 10
+    }
     }   
 )
 export default AnimatedStock;

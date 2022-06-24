2564ed95705d3e255153d834180396e14c923d1c
neverdivideby0 <bryan_lee00@hotmail.com>
2022-06-24 19:47:12 +0800

    added material icons to bottom tab navigator

--- src/screens/mainStack.js
+++ src/screens/mainStack.js
@@ -8,20 +8,44 @@ import { createStackNavigator } from '@react-navigation/stack';
 
 import HomeScreen from './main/HomeScreen'
 import SettingsScreen from './main/SettingsScreen'
+import LeaderboardScreen from './main/LeaderboardScreen'
+
+import Icons from 'react-native-vector-icons/FontAwesome';
+
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+
+
 
 const Tab = createBottomTabNavigator();
 
 const BottomTabNavigator = () => {
     return (
       <Tab.Navigator>
-        <Tab.Screen name="HomeScreen" component={HomeScreen} />
-        <Tab.Screen name="SettingsScreen" component={SettingsScreen} />
+        <Tab.Screen name="HomeScreen" component={HomeScreen} options={{
+          tabBarLabel: 'Home',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="home" color={color} size={26} />
+          ),
+        }} />
+
+        <Tab.Screen name="LeaderboardScreen" component={LeaderboardScreen} options={{ 
+            tabBarLabel: 'leaderboard',
+            tabBarIcon: ({ color }) => (
+            <MaterialIcons name="leaderboard" color={color} size={26} />
+          ),
+        }}
+         />
+
+        <Tab.Screen name="SettingsScreen" component={SettingsScreen} options={{ 
+            tabBarLabel: 'Settings',
+            tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="account" color={color} size={26} />
+          ),
+        }}
+         />
       </Tab.Navigator>
     );
   };
   
-  export default BottomTabNavigator;
-
-
-
-
+export default BottomTabNavigator;
